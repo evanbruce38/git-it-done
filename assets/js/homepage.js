7c45a8e2aa49e29b270f4ea215da851ca4981608
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-07-17 15:34:37 -0400

    added language search feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,5 @@
 var userFormEl = document.querySelector('#user-form');
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
@@ -18,6 +19,18 @@ var formSubmitHandler = function(event) {
     nameInputEl.value = '';
   } else {
     alert('Please enter a GitHub username');
+  }
+};
+
+var buttonClickHandler = function(event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
   }
 };
 
@@ -42,6 +55,23 @@ var getUserRepos = function(user) {
     .catch(function(error) {
       alert('Unable to connect to GitHub');
     });
+};
+
+var getFeaturedRepos = function(language) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    // request was succesful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: Github User Not Found');
+    }
+  });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -92,3 +122,4 @@ var displayRepos = function(repos, searchTerm) {
 
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
